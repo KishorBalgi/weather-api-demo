8f1583fd0686626dd1bdb96f14616f4d4b69ae61
Kishor Balgi <kishorbalgi@gmail.com>
2024-01-04 19:31:20 +0530

    fix: Minor bug

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const app = express();
+const path = require("path");
 dotenv.config();
 
 const weatherRouter = require("./routes/weather");
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // app.use("/", (req, res, next) => {
-//   res.sendFile(__dirname + "/views/index.html");
+//   res.sendFile(__dirname + "/public/index.html");
 // });
+
 app.use("/", weatherRouter);
 
 // Error handling:
